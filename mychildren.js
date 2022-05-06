9f26743e18509f8c8a8597fc7031ce7df7713e5b
jenna-graham <jclayto2@gmail.com>
2022-05-06 14:30:36 -0700

    created created an array for the domain My children, created html layout and added CSS to it

---
+++ mychildren.js
@@ -0,0 +1,7 @@
+export const children = [{
+    name: Giyana,
+    age: 13,
+    favoriteFood: tacos,
+    image: './assets/giyana.jpg',
+    hobbies: ['making jewelry', 'acting in plays', 'hanging with friends'] 
+}];
